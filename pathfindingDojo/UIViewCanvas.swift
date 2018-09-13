45451b75e0eb7a4e39009eed6bc7d1b701273ecd
Patrick Weber <patrick-weber@outlook.com>
2018-09-13 16:53:17 +0200

    working obstacles with list

--- pathfindingDojo/UIViewCanvas.swift
+++ pathfindingDojo/UIViewCanvas.swift
@@ -13,36 +13,51 @@ import UIKit
 class UIViewCanvas: UIView {
     
     private struct Constants {
-        static var plusLineWidth: CGFloat = 1.5
+        static var plusLineWidth: CGFloat = 1
         static var plusButtonScale: CGFloat = 1
         static var halfPointShift: CGFloat = 0.5
     }
     
     override func draw(_ rect: CGRect) {
-
+        let rowAmount: Int = 20
+        let colAmount: Int = 40
+        
+        let blockWidth = Int(floor(Double(Int(bounds.width) / rowAmount)))
+        let blockHeight = Int(floor(Double(Int(bounds.height) / colAmount)))
+        let width = Int(bounds.width)
+        let height = Int(bounds.height)
+        print(blockWidth, blockHeight)
+        
         let path = UIBezierPath(rect: rect)
         UIColor.lightGray.setFill()
         path.fill()
-        row()
-        col()
-        //drawObstacles()
+        row(blockHeight: blockHeight, width: width, height: height)
+        col(blockWidth: blockWidth, width: width, height: height)
+        drawObstacles(blockWidthMult: blockWidth, blockHeightMult: blockHeight)
         
     }
     
-//    func drawObstacles() {
-//        var obstacles:[Obstacle] = []
-//        let obstacle1 = Obstacle(x: 5,y: 5)
-//
-//        obstacles.append(obstacle1)
-//
-//        let path
-//    }
+    func drawObstacles(blockWidthMult: Int, blockHeightMult: Int) {
+        var obstacles:[Obstacle] = []
 
-    func row() {
-        let currentWidth: CGFloat = 0
-        var currentHeight: CGFloat = bounds.height / 40
+        let obstacle1 = Obstacle(x: 5,y: 5)
+        obstacles.append(obstacle1)
+        let obstacle2 = Obstacle(x: 10,y: 10)
+        obstacles.append(obstacle2)
+
+        for obstacle in obstacles {
+            let rec = CGRect(x: obstacle.x * blockWidthMult, y: obstacle.y * Int(bounds.height / 40), width: Int(bounds.width / 20), height: Int(bounds.height / 40));
+            let pathObs = UIBezierPath(rect: rec)
+            UIColor.green.setFill()
+            pathObs.fill()
+        }
+    }
+
+    func row(blockHeight: Int, width: Int, height: Int) {
+        let currentWidth = 0
+        var currentHeight = blockHeight
         
-        while currentHeight < bounds.height {
+        while currentHeight < height {
             let plusPath = UIBezierPath()
             
             plusPath.lineWidth = Constants.plusLineWidth
@@ -52,22 +67,24 @@ class UIViewCanvas: UIView {
                 y: currentHeight))
             
             plusPath.addLine(to: CGPoint(
-                x: currentWidth + bounds.width,
+                x: currentWidth + width,
                 y: currentHeight))
             
             UIColor.black.setStroke()
             plusPath.stroke()
             
-            currentHeight += bounds.height / 40
+            currentHeight += height / 40
         }
     }
     
-    func col() {
+    func col(blockWidth: Int, width: Int, height: Int) {
         
-        var currentWidth1: CGFloat = bounds.width / 20
-        let currentHeight1: CGFloat = 0
+        var currentWidth1 = blockWidth
+        let currentHeight1 = 0
         
-        while currentWidth1 < bounds.width{
+        while currentWidth1 < width{
+            
+            print(currentWidth1)
         
             let plusPath1 = UIBezierPath()
         
@@ -79,12 +96,12 @@ class UIViewCanvas: UIView {
         
             plusPath1.addLine(to: CGPoint(
             x: currentWidth1,
-            y: currentHeight1 + bounds.height))
+            y: currentHeight1 + height))
         
             UIColor.black.setStroke()
             plusPath1.stroke()
         
-            currentWidth1 += bounds.width / 20
+            currentWidth1 += width / 20
         }
     }
     
